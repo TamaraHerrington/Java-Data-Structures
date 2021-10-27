dbd0ccee1cd7b58587b7741b3c02d659c7d22b63
TamaraHerrington <tamaraherrington24@gmail.com>
2021-10-27 20:12:46 +0100

    create char list

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+//not sure why this isn't working for me but will try this again later
+public class Main {
+    private static void exercise1() {
+        List<Character> leters = new List<leters>();
+        leters.add('a');
+        leters.add('b');
+        leters.add('b');
+        leters.add('c');
+        System.out.println(leters);
+        leters.add('c', 'f');
+        System.out.println(leters);
+    }
+}
