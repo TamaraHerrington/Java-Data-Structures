c85d7d3687f453142d2de88fcfd87a5e07c41cca
TamaraHerrington <tamaraherrington24@gmail.com>
2021-10-27 20:15:09 +0100

    attempt string method

---
+++ src/main/java/String.java
@@ -0,0 +1,13 @@
+public class String {
+    @Override
+    public java.lang.String toString() {
+        return super.toString();
+    }
+}
+
+//pseudocode
+
+String string = new String
+        for{
+
+                }
