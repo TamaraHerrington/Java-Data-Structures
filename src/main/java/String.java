82855caa76ece95551c61a68776dd17e4ea11890
TamaraHerrington <tamaraherrington24@gmail.com>
2021-10-27 21:40:34 +0100

    add comments to explain todo

--- src/main/java/String.java
+++ src/main/java/String.java
@@ -1,6 +1,7 @@
 
 //exercise 1 pseudocode - use map function?
 
+//convert string to upper case??? or character???
 //convert letters in string to characters that can be counted
 
 
@@ -10,7 +11,14 @@
 class countLettersInString{
     static void characterCount(String stringName){
 
+        // according to w3schools: Creating a HashMap containing char
+        // as a key and occurrences as  a value
+        HashMap<Character, Integer> charCountMap
+                = new HashMap<Character, Integer>();
+
 //turn each letter in the string into a CHAR
         char[] strArray = stringName.toCharArray();
     }
 }
+
+//loop through each char and use get() and put()
